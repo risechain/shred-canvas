b3b8d7eb3a4f507b73ea1bd8d1aba8c2dd332271
Jen <jen@robolabs.biz>
2025-05-20 05:12:06 +0800

    update main skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-"use client";
-
-import { useSidebar } from "@/components/ui";
-import { cn } from "@/lib/utils";
-import { Demo } from "./_demo/Demo";
-
 export default function Home() {
-  const { state } = useSidebar();
-
   return (
-    // Remove me!
-    <div
-      data-state={state}
-      className={cn(
-        "h-full data-[state=expanded]:ml-[300px] data-[state=collapsed]:ml-16 transition-all",
-        "data-[state=collapsed]:max-md:ml-0 data-[state=expanded]:max-md:ml-0"
-      )}
-    >
-      <Demo />
+    <div className="flex items-center justify-center">
+      <h2>I am a Skeleton Repo - Customize me!</h2>
     </div>
   );
 }
