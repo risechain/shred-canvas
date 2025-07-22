377ff5bc5385ceb230134cde1aaf040681ab64dc
SmoothBot <smoothbot123@gmail.com>
2025-07-22 16:22:28 +1000

    open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,20 @@ export const metadata: Metadata = {
   title: "RISE Shred Canvas",
   description:
     "Discover the power of the Shred API — a client made specifically to interact with Shreds on the RISE Chain!",
+  openGraph: {
+    title: "RISE Shred Canvas",
+    description:
+      "Discover the power of the Shred API — a client made specifically to interact with Shreds on the RISE Chain!",
+    images: ["/open-graph-canvas.png"],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "RISE Shred Canvas",
+    description:
+      "Discover the power of the Shred API — a client made specifically to interact with Shreds on the RISE Chain!",
+    images: ["/open-graph-canvas.png"],
+  },
   icons: {
     icon: [
       { url: "/favicon_light.svg", media: "(prefers-color-scheme: light)" },
